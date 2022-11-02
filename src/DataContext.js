1bd0827b83ffaf666fd1fe6fbbb0c039b53f9fbe
Herve Hamann <hervehamann31@gmail.com>
2022-11-02 15:56:36 +0100

    Deploy mocked Data only

--- src/DataContext.js
+++ src/DataContext.js
@@ -11,8 +11,6 @@ export const DataContext = createContext();
  */
 
 export const DataProvider = ({ children }) => {
-  const path = "http://localhost:3000";
-
   /**
    * getUserData
    * @function getUserData
@@ -22,13 +20,8 @@ export const DataProvider = ({ children }) => {
 
   async function getUserData(id) {
     let userDataMocked = USER_MAIN_DATA.find((item) => item.id === parseInt(id));
-    const res = await fetch(`${path}/user/${id}`);
-    let userData = await res.json().catch((error) => {
-      return userDataMocked;
-    });
 
-    let userDataApi = userData.data;
-    return userDataMocked ? userDataMocked : userDataApi;
+    return userDataMocked;
   }
 
   /**
@@ -40,13 +33,8 @@ export const DataProvider = ({ children }) => {
 
   async function getUserActivity(id) {
     let userActivityMocked = USER_ACTIVITY.find((item) => item.userId === parseInt(id));
-    const res = await fetch(`${path}/user/${id}/activity`);
-    let userActivity = await res.json().catch((error) => {
-      return userActivityMocked;
-    });
 
-    let userActivityApi = userActivity.data;
-    return userActivityMocked ? userActivityMocked : userActivityApi;
+    return userActivityMocked;
   }
 
   /**
@@ -58,14 +46,8 @@ export const DataProvider = ({ children }) => {
 
   async function getUserSessions(id) {
     let userSessionsMocked = USER_AVERAGE_SESSIONS.find((item) => item.userId === parseInt(id));
-    const res = await fetch(`${path}/user/${id}/average-sessions`);
-    let userSessions = await res.json().catch((error) => {
-      return userSessionsMocked;
-    });
 
-    let userSessionsApi = userSessions.data;
-
-    return userSessionsMocked ? userSessionsMocked : userSessionsApi;
+    return userSessionsMocked;
   }
 
   /**
@@ -77,13 +59,8 @@ export const DataProvider = ({ children }) => {
 
   async function getUserPerformance(id) {
     let userPerformanceMocked = USER_PERFORMANCE.find((item) => item.userId === parseInt(id));
-    const res = await fetch(`${path}/user/${id}/performance`);
-    let userPerformance = await res.json().catch((error) => {
-      return userPerformanceMocked;
-    });
-    let userPerformanceApi = userPerformance.data;
 
-    return userPerformanceMocked ? userPerformanceMocked : userPerformanceApi;
+    return userPerformanceMocked;
   }
 
   return (
